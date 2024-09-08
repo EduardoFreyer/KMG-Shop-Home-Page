1518b2ff13249e93344071630933de6fed848cb9
Eduardo Netto Freyer <eduardofreyer@hotmail.com>
2024-09-08 20:30:49 -0300

    Dropdown menu e responsividade

---
+++ README.md
@@ -0,0 +1,2 @@
+> [!IMPORTANT]
+> Após fazer o clone do repositório, dentro da pasta onde foi salvo rode o `npm install`
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import NavLinks from "./NavLinks";
+import { IoBagOutline, IoPersonOutline, IoMenuOutline, IoCloseOutline } from "react-icons/io5";
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <nav className='bg-black flex w-full h-24 font-medium items-center justify-around'>
+      <div className="z-20 p-5 md:w-auto w-full flex justify-between">
+        <h1 className="font-bold text-white text-3xl text-nowrap">KMG Shop</h1>
+        <div className="md:hidden" onClick={() => setOpen(!open)}>
+          {open ? <IoCloseOutline size={30} style={{color: "white"}} /> : <IoMenuOutline size={30} style={{color: "white"}}/>}
+        </div>
+      </div>
+      <ul className="md:flex hidden items-center gap-8">
+        <NavLinks />
+        <IoBagOutline style={{color: "white"}} size={25}/>
+        <IoPersonOutline style={{color: "white"}} size={25}/>
+      </ul>
+      {/*Mobile*/}
+      <ul className={`md:hidden bg-black absolute w-full h-full bottom-0 py-24
+        pl-4 duration-500 ${open ? "left-0" : "left-[-100%]"}`}>
+        <NavLinks />
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar;
